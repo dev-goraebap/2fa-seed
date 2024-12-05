c4045510b0b4779a9d421ec053122a3f34bce434
dev.goraebap <dev.goraebap@gmail.com>
2024-12-06 00:18:36 +0900

    Refactor: 인증 서비스
    - 회원가입 메서드 초안 작성

--- api/src/app/user/services/auth.service.ts
+++ api/src/app/user/services/auth.service.ts
@@ -1,23 +1,42 @@
 import { BadRequestException } from "@nestjs/common";
 import { nanoid } from "nanoid";
+
 import { RegisterWithPhoneDTO } from "../dto";
+import { UserEntity } from "../infra/entities";
 
 export class AuthService {
 
     constructor(
-        private readonly userRepository: any
+        private readonly userRepository: any,
+        private readonly otpService: any
     ) { }
 
-    async register(dto: RegisterWithPhoneDTO) {
+    async register(dto: RegisterWithPhoneDTO): Promise<void> {
         // STEP: 중복 검사
         const hasUser = await this.userRepository.findByUsername(dto.username);
         if (hasUser) {
             throw new BadRequestException('이미 존재하는 아이디입니다.');
         }
 
-        // STEP: 회원가입
+        // Given: 회원 엔티티 생성 필요 데이터 초기화 
         let randomNickname = '랜덤닉네임';
         let userId = nanoid(30);
+        let otpCode = nanoid(6);
 
+        // STEP: 회원 엔티티 생성
+        const user = UserEntity.create({
+            id: userId,
+            username: dto.username,
+            nickname: randomNickname,
+            password: dto.password,
+            phoneNumber: dto.phoneNumber,
+            otpCode,
+        });
+
+        // STEP: DB에 회원 데이터 저장
+        await this.userRepository.save(user);
+
+        // STEP: OTP 발송
+        await this.otpService.send(dto.phoneNumber, otpCode);
     }
 }
