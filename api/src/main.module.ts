2c81d09ef87b5a45fe6d4a10c63ef169202ae59b
dev.goraebap <dev.goraebap@gmail.com>
2024-12-06 12:08:23 +0900

    Refactor: third-party 모듈 연결

--- api/src/main.module.ts
+++ api/src/main.module.ts
@@ -2,11 +2,13 @@ import { Module } from "@nestjs/common";
 import { ApiModule } from "./api";
 import { ConfigModule } from "./shared/config";
 import { DatabaseModule } from "./shared/database";
+import { ThirdPartyModule } from "./shared/third-party";
 
 @Module({
     imports: [
         ConfigModule,
         DatabaseModule,
+        ThirdPartyModule,
         ApiModule
     ],
 })
