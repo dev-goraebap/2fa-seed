70a3cf8b0d924e235242d8967bb4bdf3287d041e
dev.goraebap <dev.goraebap@gmail.com>
2024-12-06 23:36:20 +0900

    Refactor: OTP 서비스 수정
    - 이메일 OTP 전송 메서드 추가
    - 모바일 OTP 전송 메서드 추가

--- api/src/shared/third-party/otp.service.ts
+++ api/src/shared/third-party/otp.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class OtpService {
-    async send(phoneNumber: string, otpCode: string) {
+    async sendToEmail(email: string, otpCode: string) {
+        throw new Error("Method not implemented.");
+    }
+
+    async sendToPhone(phoneNumber: string, otpCode: string) {
         throw new Error("Method not implemented.");
     }
 }
