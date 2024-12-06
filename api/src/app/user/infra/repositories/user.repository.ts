d19407aeb8b6e0ea20ca9c0c8dc02b915590d458
dev.goraebap <dev.goraebap@gmail.com>
2024-12-06 20:14:37 +0900

    Feat: 회원 리파지토리 추가

---
+++ api/src/app/user/infra/repositories/user.repository.ts
@@ -0,0 +1,30 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { ActionRepository } from "src/shared/database";
+import { Repository } from "typeorm";
+import { UserEntity } from "../entities";
+
+@Injectable()
+export class UserRepository implements ActionRepository<UserEntity> {
+
+    constructor(
+        @InjectRepository(UserEntity)
+        private readonly userRepository: Repository<UserEntity>,
+    ) { }
+
+    async findUserByUsername(username: string) {
+        return this.userRepository.findOne({
+            where: {
+                username
+            }
+        });
+    }
+
+    async save(entity: UserEntity): Promise<void> {
+        await this.userRepository.save(entity);
+    }
+
+    async remove(entity: UserEntity): Promise<void> {
+        await this.userRepository.remove(entity);
+    }
+}
