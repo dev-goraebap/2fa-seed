71e1d7caa27cf1df2cd571eb047b8f92b8646754
dev.goraebap <dev.goraebap@gmail.com>
2024-12-06 20:16:06 +0900

    Feat: 회원 도메인 모듈 연결

--- api/src/api/api.module.ts
+++ api/src/api/api.module.ts
@@ -1,11 +1,16 @@
 import { Module } from "@nestjs/common";
+
+import { UserModule } from "src/app/user";
 import { AppController } from "./controllers/app.controller";
 import { AuthController } from "./controllers/auth.controller";
 
 @Module({
+    imports: [
+        UserModule
+    ],
     controllers: [
         AppController,
         AuthController
-    ]
+    ],
 })
 export class ApiModule {}
