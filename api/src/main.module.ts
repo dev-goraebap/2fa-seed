d552a9d7afbbd9c347b5027e0a9cfe741e6cb05e
dev.goraebap <dev.goraebap@gmail.com>
2024-12-08 03:31:00 +0900

    Feat: 인증 모듈 추가

--- api/src/main.module.ts
+++ api/src/main.module.ts
@@ -1,13 +1,16 @@
 import { Module } from "@nestjs/common";
+
 import { ApiModule } from "./api";
 import { ConfigModule } from "./shared/config";
 import { DatabaseModule } from "./shared/database";
+import { SecurityModule } from "./shared/security";
 import { ThirdPartyModule } from "./shared/third-party";
 
 @Module({
     imports: [
         ConfigModule,
         DatabaseModule,
+        SecurityModule,
         ThirdPartyModule,
         ApiModule
     ],
