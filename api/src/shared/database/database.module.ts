1a2fa316671dd29d0eb0524fe4a1c0071e88b3bb
dev.goraebap <dev.goraebap@gmail.com>
2024-12-07 21:30:54 +0900

    Feat:  db 설정 업데이트

--- api/src/shared/database/database.module.ts
+++ api/src/shared/database/database.module.ts
@@ -1,6 +1,7 @@
 import { Module } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { SnakeNamingStrategy } from "typeorm-naming-strategies";
 import { EnvConfig } from "../config";
 
 @Module({
@@ -13,7 +14,11 @@ import { EnvConfig } from "../config";
                     port: configService.get('DB_PORT'),
                     database: configService.get('DB_NAME'),
                     username: configService.get('DB_USERNAME'),
-                    password: configService.get('DB_PASSWORD')
+                    password: configService.get('DB_PASSWORD'),
+                    logging: configService.get('DB_LOGGING') === 'true' ? true : false,
+                    autoLoadEntities: true,
+                    synchronize: true,
+                    namingStrategy: new SnakeNamingStrategy()
                 }
             },
             inject: [ConfigService]
