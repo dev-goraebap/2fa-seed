667beb15a714180666c0f44b391ba4389b976c2c
dev.goraebap <dev.goraebap@gmail.com>
2024-12-07 21:40:14 +0900

    Feat: 회원 엔티티 업데이트

--- api/src/app/user/infra/entities/user.entity.ts
+++ api/src/app/user/infra/entities/user.entity.ts
@@ -1,8 +1,8 @@
 import { USER_RULES, UserStatus } from "domain-shared/user";
-import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryColumn } from "typeorm";
+import { Column, Entity, PrimaryColumn } from "typeorm";
 
 import { plainToInstance } from "class-transformer";
-import { } from 'domain-shared/user';
+import { hashPassword } from "src/shared/security";
 
 @Entity({ name: 'users', comment: '사용자 정보' })
 export class UserEntity {
@@ -13,13 +13,13 @@ export class UserEntity {
     @Column({ comment: '닉네임', length: USER_RULES.nickname.max })
     readonly nickname: string;
 
-    @Column({ comment: '이메일', length: 50, unique: true })
+    @Column({ comment: '이메일', length: USER_RULES.email.max, unique: true })
     readonly email: string;
 
-    @Column({ comment: '비밀번호', length: USER_RULES.password.max, nullable: true })
+    @Column({ comment: '비밀번호', length: USER_RULES.password.hashMax, nullable: true })
     readonly password: string;
 
-    @Column({ comment: '전화번호', length: USER_RULES.phoneNumber.max, unique: true })
+    @Column({ comment: '전화번호', length: USER_RULES.phoneNumber.max, unique: true, nullable: true })
     readonly phoneNumber: string;
 
     @Column({ comment: 'OTP코드', length: USER_RULES.otpCode.max, nullable: true })
@@ -32,12 +32,7 @@ export class UserEntity {
         return plainToInstance(UserEntity, {
             ...param,
             status: UserStatus.PENDING,
+            password: hashPassword(param.password),
         } as UserEntity);
-    }
-
-    @BeforeInsert()
-    @BeforeUpdate()
-    private beforeCreateOrUpdate() {
-        // TODO: 비밀번호 암호화 처리
     }
 }
