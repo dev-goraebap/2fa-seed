e94b1bcf9d144e9abe4a70692adfc7a764f929aa
dev.goraebap <dev.goraebap@gmail.com>
2024-12-12 02:10:21 +0900

    Feat: 프로필 페이지 추가

---
+++ client/src/pages/private/profile/ui/profile.page.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'profile-page',
+    imports: [],
+    template: `
+    <div class="p-4">
+        프로필 페이지
+    </div>
+    `
+})
+export class ProfilePage { }
