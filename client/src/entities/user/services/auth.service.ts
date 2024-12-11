6eb433e567c5cfbd188f5352ed9a2a5f2a31bd99
dev.goraebap <dev.goraebap@gmail.com>
2024-12-12 03:07:07 +0900

    Feat: 로그인, 회원가입, OTP 인증 테스트 코드 작성

--- client/src/entities/user/services/auth.service.ts
+++ client/src/entities/user/services/auth.service.ts
@@ -1,8 +1,7 @@
-import { HttpClient } from "@angular/common/http";
-import { inject, Injectable, signal } from "@angular/core";
-import { delay, of, tap } from "rxjs";
+import { Injectable } from "@angular/core";
+import { delay, of, throwError } from "rxjs";
 
-import { RegisterDTO } from "domain-shared/user";
+import { LoginDTO, RegisterDTO, VerifyOtpDTO } from "domain-shared/user";
 
 export type CommonState = {
     isFetching: boolean;
@@ -15,27 +14,81 @@ export type CommonState = {
 })
 export class AuthService {
 
-    private readonly httpClient = inject(HttpClient);
-    private readonly state = signal<CommonState>({
-        isFetching: false,
-        error: null,
-        data: null,
-    });
+    login(dto: LoginDTO) {
+        let result = null;
+
+        const result01 = {
+            code: 'LOGIN_SUCCESS',
+            data: {
+                accessToken: 'ACCESS_TOKEN',
+                refreshToken: 'REFRESH_TOKEN',
+                expiresAt: Date.now() + 3600
+            }
+        };
+        const result02 = {
+            code: 'NEED_OTP',
+            data: null
+        };
+
+        const probability = Math.random();
+        if (probability < 0.5) {
+            result = result01;
+        } else if (probability < 0.8) {
+            result = result02;
+        } else {
+            return throwError(() => '아이디 또는 비밀번호가 틀려요');
+        }
+
+        return of(result).pipe(delay(1000));
+    }
 
     register(dto: RegisterDTO) {
-        this.state.update(state => ({
-            ...state,
-            isFetching: true,
-        }));
+        let result = null;
 
-        return this.httpClient.post('http://localhost:8000/api/v1/auth/register', dto).pipe(
-            delay(1000),
-            tap(() => {
-                this.state.update(state => ({
-                    ...state,
-                    isFetching: false,
-                }));
-            })
-        );
+        const result01 = {
+            code: 'REGISTER_SUCCESS',
+            data: {
+                accessToken: 'ACCESS_TOKEN',
+                refreshToken: 'REFRESH_TOKEN',
+                expiresAt: Date.now() + 3600
+            }
+        };
+        const result02 = {
+            code: 'NEED_OTP',
+            data: null
+        };
+
+        const probability = Math.random();
+        if (probability < 0.5) {
+            result = result01;
+        } else if (probability < 0.8) {
+            result = result02;
+        } else {
+            return throwError(() => '이미 사용중인 아이디에요.');
+        }
+
+        return of(result).pipe(delay(1000));
+    }
+
+    verifyOtp(otp: VerifyOtpDTO) {
+        let result = null;
+
+        const result01 = {
+            code: 'VERIFY_OTP_SUCCESS',
+            data: {
+                accessToken: 'ACCESS_TOKEN',
+                refreshToken: 'REFRESH_TOKEN',
+                expiresAt: Date.now() + 3600
+            }
+        };
+
+        const probability = Math.random();
+        if (probability < 0.5) {
+            result = result01;
+        } else {
+            return throwError(() => 'OTP 인증에 실패했어요.');
+        }
+
+        return of(result).pipe(delay(1000));
     }
 }
