7ab53a7f6175521be239e73368009492f41a76b6
dev.goraebap <dev.goraebap@gmail.com>
2024-12-12 21:46:18 +0900

    Feat: 로그인 페이지 업데이트

--- client/src/pages/public/login/ui/login.page.ts
+++ client/src/pages/public/login/ui/login.page.ts
@@ -1,8 +1,8 @@
-import { Component, inject, signal, WritableSignal } from "@angular/core";
+import { Component, inject, Signal, signal, viewChild, WritableSignal } from "@angular/core";
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { catchError, EMPTY, finalize, tap } from "rxjs";
 
-import { Router } from "@angular/router";
+import { Router, RouterLink } from "@angular/router";
 import { LoginDTO, VerifyOtpDTO } from 'domain-shared/user';
 import { AuthService } from "src/entities/user";
 import { LoginForm } from "src/features/user/login";
@@ -12,28 +12,35 @@ import { OtpVerifyForm } from "src/features/user/verify-otp";
     selector: 'login-page',
     imports: [
         LoginForm,
-        OtpVerifyForm
+        OtpVerifyForm,
+        RouterLink
     ],
     template: `
     <div class="p-4">
         @if(!needVerifyOtp()) {
-            <login-form 
-                [isFetching]="isFetching()" 
-                (notify)="login($event)"
-            />
+            <div class="text-2xl font-semibold">
+                데모앱이에요오.<br/>
+                이메일과 비밀번호를 입력하세요.
+            </div>
+            <br/>
+            <login-form #loginForm (notify)="login($event)"/>
+            <br/>
+            <div class="flex justify-between px-4">
+                <a routerLink="/find-password" class="text-blue-500">비밀번호 찾기</a>
+                <a routerLink="/register" class="text-blue-500">회원가입</a>
+            </div>
         } @else {
-            <otp-verify-form 
-                [isFetching]="isFetching()" 
-                (notify)="verifyOtp($event)"
-            />
+            <div class="text-2xl font-semibold">
+                이런! 추가인증이 필요해요.<br/>
+                OTP 코드를 입력해주세요.
+            </div>
+            <br/>
+            <otp-verify-form #otpVerifyForm (notify)="verifyOtp($event)"/>
         }
     </div>
     `
 })
 export class LoginPage {
-
-    protected readonly isFetching: WritableSignal<boolean> = signal(false);
-
     /**
      * 현재 로그인 페이지에서 추가로 OTP 인증이 필요한지 판단하는 플래그
      */
@@ -44,13 +51,14 @@ export class LoginPage {
      */
     protected readonly tempLoginFormData: WritableSignal<LoginDTO | undefined> = signal(undefined);
 
-    private readonly snackBar = inject(MatSnackBar);
-    private readonly authService = inject(AuthService);
-    private readonly router = inject(Router);
+    private readonly snackBar: MatSnackBar = inject(MatSnackBar);
+    private readonly authService: AuthService = inject(AuthService);
+    private readonly router: Router = inject(Router);
+
+    private readonly loginFormEl: Signal<LoginForm> = viewChild.required('loginForm');
+    private readonly otpVerifyFormEl: Signal<OtpVerifyForm> = viewChild.required('otpVerifyForm');
 
     protected login(dto: LoginDTO) {
-        this.isFetching.set(true);
-
         this.authService.login(dto).pipe(
             tap((res) => {
                 if (res.code === 'LOGIN_SUCCESS') {
@@ -67,13 +75,11 @@ export class LoginPage {
                 });
                 return EMPTY;
             }),
-            finalize(() => this.isFetching.set(false))
+            finalize(() => this.loginFormEl().completeFetching())
         ).subscribe();
     }
 
     protected verifyOtp(otp: string) {
-        this.isFetching.set(true);
-
         const dto: VerifyOtpDTO = {
             username: this.tempLoginFormData()!.username,
             otp
@@ -88,7 +94,7 @@ export class LoginPage {
                 });
                 return EMPTY;
             }),
-            finalize(() => this.isFetching.set(false))
+            finalize(() => this.otpVerifyFormEl().completeFetching())
         ).subscribe();
     }
 }
