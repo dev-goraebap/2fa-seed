dbb4653e29d642ab8a1e48f29839a98c5bf05399
dev.goraebap <dev.goraebap@gmail.com>
2024-12-14 01:18:58 +0900

    Feat: 프로필 수정 페이지 추가

---
+++ client/src/pages/private/edit-profile/ui/edit-profile.page.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'edit-profile-page',
+    imports: [
+        
+    ],
+    template: ``
+})
+export class EditProfilePage {
+
+}
