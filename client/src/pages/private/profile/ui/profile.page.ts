05bb23ff9a23d59c562f7e81c5ee119743b4fb0f
dev.goraebap <dev.goraebap@gmail.com>
2024-12-14 01:18:13 +0900

    Feat: 프로필 페이지 추가

--- client/src/pages/private/profile/ui/profile.page.ts
+++ client/src/pages/private/profile/ui/profile.page.ts
@@ -1,12 +1,44 @@
-import { Component } from "@angular/core";
+import { NgClass } from "@angular/common";
+import { Component, inject } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { RouterLink } from "@angular/router";
+import { UserService } from "src/entities/user";
 
 @Component({
     selector: 'profile-page',
-    imports: [],
+    imports: [
+        MatButtonModule,
+        RouterLink,
+        NgClass
+    ],
     template: `
-    <div class="p-4">
-        프로필 페이지
+    <div data-anim="slideRight" class="p-4">
+        <div class="flex justify-start gap-4">
+            <div class="rounded w-24 h-24 bg-blue-500"></div>
+            <div>
+                <div class="text-xl">고래밥</div>
+                <div class="text-sm mt-1">dev.goraebap gmail.com</div>
+                <div class="text-sm mt-1">가입일자: 2024-10-10</div>
+            </div>
+        </div>
+        <div class="mt-4">
+            <button mat-stroked-button class="w-full" routerLink="/profile/edit">프로필 수정</button>
+        </div>
+    </div>
+    <div class="border-2"></div>
+    <div class="flex justify-center p-10">
+        <button class="text-red-400 motion" [ngClass]="{
+        'motion-preset-confetti': clicked    
+    }" (click)="onClick()">회원 탈퇴</button>
     </div>
     `
 })
-export class ProfilePage { }
+export class ProfilePage {
+
+    clicked = false;
+    private readonly userService = inject(UserService);
+
+    onClick() {
+        this.clicked = !this.clicked;
+    }
+}
