c0469dd958e1b2587186df0940324c450d545c3f
dev.goraebap <dev.goraebap@gmail.com>
2024-12-14 16:15:13 +0900

    Feat: 회원 서비스 업데이트

--- client/src/entities/user/services/user.service.ts
+++ client/src/entities/user/services/user.service.ts
@@ -1,11 +1,36 @@
-import { HttpClient } from "@angular/common/http";
-import { inject, Injectable } from "@angular/core";
+import { Injectable, Signal, signal, WritableSignal } from "@angular/core";
+import { delay, Observable, of, tap } from "rxjs";
 
+import { ProfileResultDTO } from "domain-shared/user";
 
 @Injectable({
     providedIn: 'root'
 })
 export class UserService {
 
-    private readonly httpClient = inject(HttpClient);
+    private readonly _isFetched: WritableSignal<boolean> = signal(false);
+    private readonly _data: WritableSignal<ProfileResultDTO | undefined> = signal(undefined);
+
+    readonly isFetched: Signal<boolean> = this._isFetched.asReadonly();
+    readonly data: Signal<ProfileResultDTO | undefined> = this._data.asReadonly();
+
+    initProfile(): Observable<ProfileResultDTO> {
+        return of({
+            id: '1',
+            nickname: 'test',
+            email: 'test@test.com',
+            createdAt: new Date()
+        }).pipe(
+            delay(1000),
+            tap(profile => {
+                console.log(profile);
+                const probability = Math.random();
+                if (probability < 0.5) {
+                    throw new Error('프로필 조회에 실패하였어요.');
+                }
+                this._data.set(profile);
+                this._isFetched.set(true);
+            }),
+        );
+    }
 }
