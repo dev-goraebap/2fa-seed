4b15cf69d946b10cd74cab706f4a42430fb50653
dev.goraebap <dev.goraebap@gmail.com>
2024-12-14 16:14:57 +0900

    Feat: 프로필 페이지 업데이트

--- client/src/pages/private/profile/ui/profile.page.ts
+++ client/src/pages/private/profile/ui/profile.page.ts
@@ -1,44 +1,50 @@
-import { NgClass } from "@angular/common";
-import { Component, inject } from "@angular/core";
+import { afterNextRender, Component, inject, Signal } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
-import { RouterLink } from "@angular/router";
-import { UserService } from "src/entities/user";
+import { Router, RouterLink } from "@angular/router";
+import { ProfileResultDTO } from "domain-shared/user";
+import { catchError, EMPTY } from "rxjs";
+import { ProfileCard, UserService } from "src/entities/user";
+import { FetchingScreen } from "src/shared/ui";
 
 @Component({
     selector: 'profile-page',
     imports: [
         MatButtonModule,
         RouterLink,
-        NgClass
+        ProfileCard,
+        FetchingScreen,
     ],
     template: `
-    <div data-anim="slideRight" class="p-4">
-        <div class="flex justify-start gap-4">
-            <div class="rounded w-24 h-24 bg-blue-500"></div>
-            <div>
-                <div class="text-xl">고래밥</div>
-                <div class="text-sm mt-1">dev.goraebap gmail.com</div>
-                <div class="text-sm mt-1">가입일자: 2024-10-10</div>
-            </div>
+    <fetching-screen [isFetched]="isFetched()">
+        <div data-anim="slideRight" class="p-4">
+            <profile-card [profile]="profile()"/>
+            <button mat-stroked-button class="w-full mt-4" routerLink="/profile/edit">프로필 수정</button>
         </div>
-        <div class="mt-4">
-            <button mat-stroked-button class="w-full" routerLink="/profile/edit">프로필 수정</button>
-        </div>
-    </div>
-    <div class="border-2"></div>
-    <div class="flex justify-center p-10">
-        <button class="text-red-400 motion" [ngClass]="{
-        'motion-preset-confetti': clicked    
-    }" (click)="onClick()">회원 탈퇴</button>
-    </div>
+        <div class="border-2"></div>
+    </fetching-screen>
     `
 })
 export class ProfilePage {
 
-    clicked = false;
-    private readonly userService = inject(UserService);
+    private readonly userService: UserService = inject(UserService);
+    private readonly router: Router = inject(Router);
 
-    onClick() {
-        this.clicked = !this.clicked;
+    protected readonly isFetched: Signal<boolean> = this.userService.isFetched;
+    protected readonly profile: Signal<ProfileResultDTO | undefined> = this.userService.data;
+
+    constructor() {
+        afterNextRender(() => {
+            this.userService.initProfile().pipe(
+                catchError(err => {
+                    console.error(err);
+                    this.router.navigateByUrl('/error', {
+                        state: {
+                            message: err
+                        }
+                    });
+                    return EMPTY;
+                })
+            ).subscribe();
+        });
     }
 }
