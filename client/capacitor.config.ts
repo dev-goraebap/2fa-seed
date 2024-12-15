72567ad5b5ebffb4e15a1e450d253181ceba937b
dev.goraebap <dev.goraebap@gmail.com>
2024-12-16 05:14:27 +0900

    Chore: 캐패시터 설정 업데이트

--- client/capacitor.config.ts
+++ client/capacitor.config.ts
@@ -3,7 +3,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'dev.goraebap.twofaseed',
   appName: '2fa-seed.client',
-  webDir: 'dist/client/browser'
+  webDir: 'dist/2fa-seed-client/browser',
+  bundledWebRuntime: true
 };
 
 export default config;
