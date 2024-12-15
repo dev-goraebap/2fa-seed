c5388dcd22fd4c800525faa89dc14c9cde661867
dev.goraebap <dev.goraebap@gmail.com>
2024-12-16 00:09:31 +0900

    Feat: api 모듈 업데이트

--- api/src/api/api.module.ts
+++ api/src/api/api.module.ts
@@ -1,8 +1,12 @@
 import { Module } from "@nestjs/common";
+import { APP_GUARD } from "@nestjs/core";
 
 import { UserModule } from "src/app/user";
+
 import { AppController } from "./controllers/app.controller";
 import { AuthController } from "./controllers/auth.controller";
+import { UserController } from "./controllers/user.controller";
+import { AuthGuard } from "./guards";
 
 @Module({
     imports: [
@@ -10,7 +14,11 @@ import { AuthController } from "./controllers/auth.controller";
     ],
     controllers: [
         AppController,
-        AuthController
+        AuthController,
+        UserController
+    ],
+    providers: [
+        { provide: APP_GUARD, useClass: AuthGuard }
     ],
 })
-export class ApiModule {}
+export class ApiModule { }
