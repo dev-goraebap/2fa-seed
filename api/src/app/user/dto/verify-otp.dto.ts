786c97104ddbb1dcd8cd5e80b57523e0d6364569
dev.goraebap <dev.goraebap@gmail.com>
2024-12-15 22:16:15 +0900

    Feat: otp 검증 dto 업데이트

--- api/src/app/user/dto/verify-otp.dto.ts
+++ api/src/app/user/dto/verify-otp.dto.ts
@@ -1,24 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsIn, IsNotEmpty, Length, Matches } from 'class-validator';
+import { IsNotEmpty, Length, Matches } from 'class-validator';
 
-import { VerifyOtpDTO as TVerifyOtpDTO, USER_RULES, UsernameTypes } from 'domain-shared/user';
-import { CheckUsername } from '../decorators';
+import { VerifyOtpDTO as TVerifyOtpDTO, USER_RULES } from 'domain-shared/user';
 
 export class VerifyOtpDTO implements TVerifyOtpDTO {
 
-    @IsNotEmpty({ message: '아이디가 비어있습니다.' })
-    @CheckUsername()
-    @ApiProperty({ description: '로그인 아이디' })
-    readonly username: string;
+    @IsNotEmpty({ message: '이메일이 비어있습니다.' })
+    @Length(USER_RULES.email.min, USER_RULES.email.max, { message: USER_RULES.email.lengthErrMsg })
+    @Matches(USER_RULES.email.regex, { message: USER_RULES.email.regexErrMsg })
+    @ApiProperty({ description: '이메일' })
+    readonly email: string;
 
     @IsNotEmpty({ message: 'OTP 코드가 비어있습니다.' })
     @Length(USER_RULES.otp.min, USER_RULES.otp.max, { message: USER_RULES.otp.lengthErrMsg })
     @Matches(USER_RULES.otp.regex, { message: USER_RULES.otp.regexErrMsg })
     @ApiProperty({ description: 'OTP 코드' })
     readonly otp: string;
-
-    @IsNotEmpty({ message: '아이디 유형이 올바르지 않습니다.' })
-    @IsIn(Object.values(UsernameTypes), { message: '아이디 유형이 올바르지 않습니다.' })
-    @ApiProperty({ description: '로그인 아이디 유형', enum: UsernameTypes })
-    readonly type: UsernameTypes;
 }
