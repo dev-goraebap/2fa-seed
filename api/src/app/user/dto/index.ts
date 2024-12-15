841910d7dfea7ae60bbbe1f3b22a2a6c1647eed3
dev.goraebap <dev.goraebap@gmail.com>
2024-12-16 00:07:59 +0900

    Feat: 프로필 조회 dto 추가

--- api/src/app/user/dto/index.ts
+++ api/src/app/user/dto/index.ts
@@ -2,4 +2,5 @@ export * from './login.dto';
 export * from './register.dto';
 export * from './res/auth-result.dto';
 export * from './res/email-duplicate-check-result.dto';
+export * from './res/profile-result.dto';
 export * from './verify-otp.dto';
---
+++ api/src/app/user/dto/res/profile-result.dto.ts
@@ -0,0 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { plainToInstance } from 'class-transformer';
+
+import { ProfileResultDTO as TProfileResultDTO } from 'domain-shared/user';
+
+import { UserEntity } from '../../infra/entities';
+
+export class ProfileResultDTO implements TProfileResultDTO {
+    @ApiProperty({ description: '사용자 ID' })
+    readonly id: string;
+
+    @ApiProperty({ description: '사용자 닉네임' })
+    readonly nickname: string;
+
+    @ApiProperty({ description: '사용자 이메일' })
+    readonly email: string;
+
+    @ApiProperty({ description: '사용자 생성일' })
+    readonly createdAt: Date;
+
+    static from(user: UserEntity): ProfileResultDTO {
+        return plainToInstance(ProfileResultDTO, {
+            id: user.id,
+            nickname: user.nickname,
+            email: user.email,
+            createdAt: user.createdAt,
+        } as ProfileResultDTO);
+    }
+}
