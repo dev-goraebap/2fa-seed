06a3be1f2e9c06ef0fff891ad41fff735186a627
dev.goraebap <dev.goraebap@gmail.com>
2024-12-18 22:55:33 +0900

    Feat: 사용자 토큰 모델 업데이트

--- api/src/app/user/models/user-token.model.ts
+++ api/src/app/user/models/user-token.model.ts
@@ -1,20 +1,23 @@
 import { plainToInstance } from "class-transformer";
+import { nanoid } from "nanoid";
 
-import { BaseFirebaseModel } from "src/shared/third-party";
+import { FirebaseModel } from "src/shared/third-party";
 
-export class UserTokenModel extends BaseFirebaseModel {
+export class UserTokenModel extends FirebaseModel {
 
     /**
      * @description 사용자 리프레시토큰 만료시간 (7일)
      */
     static readonly EXPIRES_TIME: number = 1000 * 60 * 24 * 7;
 
+    readonly id: string;
     readonly userId: string;
     readonly refreshToken: string;
     readonly expiresAt: Date;
 
     static create(param: Pick<UserTokenModel, 'userId' | 'refreshToken'>): UserTokenModel {
         return plainToInstance(UserTokenModel, {
+            id: nanoid(30),
             userId: param.userId,
             refreshToken: param.refreshToken,
             expiresAt: new Date(Date.now() + UserTokenModel.EXPIRES_TIME)
