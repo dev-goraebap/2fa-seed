2da9d03ede6d5b18b4f84acec3d3c394ad1dc431
dev.goraebap <dev.goraebap@gmail.com>
2024-12-19 16:49:01 +0900

    Feat: dto 업데이트

--- api/src/app/user/dto/register.dto.ts
+++ api/src/app/user/dto/register.dto.ts
@@ -6,7 +6,7 @@ import { RegisterDTO as TRegisterDTO, USER_RULES } from 'domain-shared/user';
 export class RegisterDTO implements TRegisterDTO {
     @IsNotEmpty({ message: '이메일은 필수 입력 항목입니다.' })
     @IsEmail({}, { message: '이메일 형식이 올바르지 않습니다.' })
-    @ApiProperty({ description: '이메일', example: 'test@test.com' })
+    @ApiProperty({ description: '이메일', example: 'test@gmail.com' })
     readonly email: string;
 
     @IsNotEmpty({ message: '비밀번호는 필수 입력 항목입니다.' })
--- api/src/app/user/dto/verify-otp.dto.ts
+++ api/src/app/user/dto/verify-otp.dto.ts
@@ -8,12 +8,12 @@ export class VerifyOtpDTO implements TVerifyOtpDTO {
     @IsNotEmpty({ message: '이메일이 비어있습니다.' })
     @Length(USER_RULES.email.min, USER_RULES.email.max, { message: USER_RULES.email.lengthErrMsg })
     @Matches(USER_RULES.email.regex, { message: USER_RULES.email.regexErrMsg })
-    @ApiProperty({ description: '이메일' })
+    @ApiProperty({ description: '이메일', example: 'test@gmail.com' })
     readonly email: string;
 
     @IsNotEmpty({ message: 'OTP 코드가 비어있습니다.' })
     @Length(USER_RULES.otp.min, USER_RULES.otp.max, { message: USER_RULES.otp.lengthErrMsg })
     @Matches(USER_RULES.otp.regex, { message: USER_RULES.otp.regexErrMsg })
-    @ApiProperty({ description: 'OTP 코드' })
+    @ApiProperty({ description: 'OTP 코드', example: '123456' })
     readonly otp: string;
 }
