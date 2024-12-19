8a7d2ad9db9d07ac81c847486c4d6f8160ec19b5
dev.goraebap <dev.goraebap@gmail.com>
2024-12-19 16:47:56 +0900

    Feat: retry-otp dto 추가

--- shared/user/dto/index.ts
+++ shared/user/dto/index.ts
@@ -3,4 +3,5 @@ export * from './register.dto';
 export * from './res/auth-result.dto';
 export * from './res/email-duplicate-check-result.dto';
 export * from './res/profile-result.dto';
+export * from './retry-otp.dto';
 export * from './verify-otp.dto';
---
+++ shared/user/dto/retry-otp.dto.ts
@@ -0,0 +1,3 @@
+export type RetryOtpDTO = {
+    readonly email: string;
+}
