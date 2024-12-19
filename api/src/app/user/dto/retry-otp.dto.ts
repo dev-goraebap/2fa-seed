27cd6febec3b50916f00f28769cceebdbc7da552
dev.goraebap <dev.goraebap@gmail.com>
2024-12-19 16:48:14 +0900

    Feat: retry otp dto 추가

---
+++ api/src/app/user/dto/retry-otp.dto.ts
@@ -0,0 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty } from 'class-validator';
+import { RetryOtpDTO as TRetryOtpDTO } from 'domain-shared/user';
+
+export class RetryOtpDTO implements TRetryOtpDTO {
+
+    @IsNotEmpty({ message: '이메일은 필수 입력 항목입니다.' })
+    @IsEmail({}, { message: '이메일 형식이 올바르지 않습니다.' })
+    @ApiProperty({ description: '이메일', example: 'test@gmail.com' })
+    readonly email: string;
+}
