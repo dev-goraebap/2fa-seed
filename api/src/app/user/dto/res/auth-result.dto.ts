e9c9cdb4ff28dacfe56c5cd9eb6898e6b1ce88cc
dev.goraebap <dev.goraebap@gmail.com>
2024-12-20 16:43:35 +0900

    Refactor: auth result dto

--- api/src/app/user/dto/res/auth-result.dto.ts
+++ api/src/app/user/dto/res/auth-result.dto.ts
@@ -14,10 +14,7 @@ export class AuthResultDTO implements TAuthResultDTO {
     @ApiProperty({ description: '액세스 토큰 만료시간' })
     readonly expiresIn: number | null;
 
-    /**
-     * 서비스 -> 컨트롤러로 전달 될 때 만 사용되는 필드입니다.
-     * 클라이언트로 전달 할 때 `toResponse` 메서드를 통해 변환하여 전달합니다.
-     */
+    @ApiProperty({ description: '리프레시토큰' })
     readonly refreshToken: string | null;
 
     static fromSuccess(accessToken: string, refreshToken: string) {
@@ -34,13 +31,5 @@ export class AuthResultDTO implements TAuthResultDTO {
             accessToken: null,
             refreshToken: null
         } as AuthResultDTO);
-    }
-
-    toResponse(): TAuthResultDTO {
-        return {
-            status: this.status,
-            accessToken: this.accessToken,
-            expiresIn: this.expiresIn
-        } as TAuthResultDTO;
     }
 }
