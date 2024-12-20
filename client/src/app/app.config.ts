79b71f003acc73e98d66472f6003e7582f8634cf
dev.goraebap <dev.goraebap@gmail.com>
2024-12-20 23:15:08 +0900

    Feat: import interceptor

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,8 +1,10 @@
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
+
+import { jwtInterceptor } from 'src/shared/libs/jwt';
 
 import { routes } from './app.routes';
 
@@ -12,6 +14,10 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
     provideAnimationsAsync(),
-    provideHttpClient()
+    provideHttpClient(
+      withInterceptors([
+        jwtInterceptor
+      ])
+    )
   ]
 };
