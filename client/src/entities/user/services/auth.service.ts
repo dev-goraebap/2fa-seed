04a6098b8e31d1e029fe32d9bfa52e580596b2fb
dev.goraebap <dev.goraebap@gmail.com>
2024-12-20 23:15:40 +0900

    Feat: update auth service

--- client/src/entities/user/services/auth.service.ts
+++ client/src/entities/user/services/auth.service.ts
@@ -1,9 +1,9 @@
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
+import { tap } from "rxjs";
 
 import { AuthResultDTO, LoginDTO, RegisterDTO, VerifyOtpDTO } from "domain-shared/user";
-import { tap } from "rxjs";
-import { localStorageStrategy } from "src/shared/session/run-outside/local-storage.strategy";
+import { LocalTokenStorage, skipAuth } from "src/shared/libs/jwt";
 
 export type CommonState = {
     isFetching: boolean;
@@ -19,19 +19,20 @@ export class AuthService {
     private readonly httpClient: HttpClient = inject(HttpClient);
 
     login(dto: LoginDTO) {
-        return this.httpClient.post<AuthResultDTO>('http://localhost:8000/api/v1/auth/login', dto).pipe(
-            tap(async res => {
-                if (res.accessToken && res.refreshToken) {
-                    console.log(res);
-                    localStorageStrategy.setAccessToken(res.accessToken);
-                    await localStorageStrategy.setRefreshToken(res.refreshToken);
+        return this.httpClient.post<AuthResultDTO>('http://localhost:8000/api/v1/auth/login', dto, { context: skipAuth() }).pipe(
+            tap(res => {
+                console.log(res);
+                if (res.accessToken && res.refreshToken && res.expiresIn) {
+                    const storage = LocalTokenStorage.getInstance();
+                    storage.setAccessToken(res.accessToken, res.expiresIn);
+                    storage.setRefreshToken(res.refreshToken);
                 }
             })
         )
     }
 
     register(dto: RegisterDTO) {
-        return this.httpClient.post<void>('http://localhost:8000/api/v1/auth/register', dto);
+        return this.httpClient.post<void>('http://localhost:8000/api/v1/auth/register', dto, { context: skipAuth() });
     }
 
     verifyOtp(otp: VerifyOtpDTO) {
@@ -50,9 +51,9 @@ export class AuthService {
             headers
         }).pipe(
             tap(res => {
-                if (res.accessToken && res.refreshToken) {
-                    localStorageStrategy.setAccessToken(res.accessToken);
-                    localStorageStrategy.setRefreshToken(res.refreshToken);
+                console.log(res);
+                if (res.accessToken) {
+
                 }
             })
         );
