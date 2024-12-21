4e406d4bce8018f340d4120dce5e4d826ad8668e
dev.goraebap <dev.goraebap@gmail.com>
2024-12-21 17:56:08 +0900

    Feat: update health check service

--- client/src/entities/health-check/health-check.service.ts
+++ client/src/entities/health-check/health-check.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
 
+import { skipAuth } from "src/shared/libs/jwt";
+
 @Injectable({
     providedIn: 'root'
 })
@@ -9,6 +11,8 @@ export class HealthCheckService {
     private readonly httpClient: HttpClient = inject(HttpClient);
 
     healthCheck() {
-        return this.httpClient.get<void>('http://localhost:8000/api/healthy');
+        return this.httpClient.get<void>('http://localhost:8000/api/healthy', {
+            context: skipAuth()
+        });
     }
 }
