18cae4c13b0a4e64c41262a77b2a75e275be15e0
dev.goraebap <dev.goraebap@gmail.com>
2024-12-21 17:56:42 +0900

    Feat: update app component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { catchError, EMPTY, tap } from 'rxjs';
+import { HealthCheckService } from 'src/entities/health-check';
+import { AuthService } from 'src/entities/user';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +13,16 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = '2fa-seed-client';
+  private readonly authService: AuthService = inject(AuthService); 
+  private readonly healthCheckService: HealthCheckService = inject(HealthCheckService);
+
+  constructor() {
+    this.healthCheckService.healthCheck().pipe(
+      tap(console.log),
+      catchError((res: HttpErrorResponse) => {
+        console.log(res.error);
+        return EMPTY;
+      })
+    ).subscribe();
+  }
 }
