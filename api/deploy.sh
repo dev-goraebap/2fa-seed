9a32e7386de063d06ecdf2aed82ced45b116df9e
dev.goraebap <dev.goraebap@gmail.com>
2024-12-24 23:13:51 +0900

    Chore: 프로젝트 배포 스크립트 작성

--- api/deploy.sh
+++ api/deploy.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
+# package.json에서 버전 추출
+VERSION=$(node -p "require('./package.json').version")
+
 # STEP: 프로젝트 빌드
 npm run build
 
 # STEP: 도커 이미지 빌드
-docker build -f ./docker/Dockerfile -t 2fa-seed-api .
-
-# STEP: 도커 컴포즈로 컨테이너 실행(테스트)
-docker-compose -f ./docker/docker-compose.yml up
+docker build -f ./docker/Dockerfile -t gcr.io/fa-seed/api:${VERSION} .
