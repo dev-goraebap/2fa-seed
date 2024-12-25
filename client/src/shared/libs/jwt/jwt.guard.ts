1402c392894a07dd4e0ecc6351cc4f24947eca01
dev.goraebap <dev.goraebap@gmail.com>
2024-12-25 23:04:54 +0900

    Refactor: jwt guard

--- client/src/shared/libs/jwt/jwt.guard.ts
+++ client/src/shared/libs/jwt/jwt.guard.ts
@@ -1,13 +1,13 @@
 import { inject } from "@angular/core";
 import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from "@angular/router";
 import { from, map } from "rxjs";
-import { LocalTokenStorage } from "./plain-functions";
+import { TokenStorage } from "./plain-functions";
 
 export function jwtGuard(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     console.log('jwt guard');
 
     const router: Router = inject(Router);
-    const tokenStorage: LocalTokenStorage = LocalTokenStorage.getInstance();
+    const tokenStorage: TokenStorage = TokenStorage.getInstance();
 
     return from(tokenStorage.getRefreshToken()).pipe(
         map(refreshToken => {
