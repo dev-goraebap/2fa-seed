2ea4e74d63416f5665c737ab47155376639d1a65
dev.goraebap <dev.goraebap@gmail.com>
2024-12-27 20:59:57 +0900

    Feat: update dto

--- shared/user/dto/register.dto.ts
+++ shared/user/dto/register.dto.ts
@@ -1,4 +1,7 @@
 export type RegisterDTO = {
     readonly email: string;
     readonly password: string;
+    readonly deviceId: string;
+    readonly deviceModel: string;
+    readonly os: string;
 }
