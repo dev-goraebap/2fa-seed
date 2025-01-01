9406d63cb3f5d821767511a0b28ff6c37f37a359
dev.goraebap <dev.goraebap@gmail.com>
2025-01-01 13:14:01 +0900

    Feat: update firebase service

--- api/src/shared/third-party/firebase/firebase.repository.ts
+++
@@ -1,4 +0,0 @@
-export interface FirebaseRepository<T> {
-    save(entity: T): Promise<void | T>;
-    remove(entity: T): Promise<void>;
-}
--- api/src/shared/third-party/firebase/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './firebase.model';
-export * from './firebase.module';
-export * from './firebase.repository';
-export * from './firebase.service';
-
