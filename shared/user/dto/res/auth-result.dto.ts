95cd1084e94c40fb650d7b6c1c351ecf7efb225c
dev.goraebap <dev.goraebap@gmail.com>
2025-01-01 13:13:17 +0900

    Feat: update dto

--- shared/user/dto/res/auth-result.dto.ts
+++ shared/user/dto/res/auth-result.dto.ts
@@ -1,6 +1,7 @@
 export enum AuthStatus {
     SUCCESS = 'SUCCESS',
-    NEED_OTP = 'NEED_OTP'
+    NEED_OTP = 'NEED_OTP',
+    NEED_SOCIAL_REGISTER = 'NEED_SOCIAL_REGISTER'
 }
 
 export type AuthResultDTO = {
