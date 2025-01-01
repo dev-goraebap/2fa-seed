e4d907c458e3ef11a6ad7c84e4217056c60a2c71
dev.goraebap <dev.goraebap@gmail.com>
2025-01-01 20:00:28 +0900

    Feat: update social auth service

--- api/src/app/user/services/social-auth.service.ts
+++ api/src/app/user/services/social-auth.service.ts
@@ -1,20 +1,22 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 
 import { SecureTokenService } from "src/shared/security";
-import { MailService } from "src/shared/third-party";
+import { FirebaseService, MailService } from "src/shared/third-party";
 
-import { AuthResultDTO } from "../dto";
+import { AuthResultDTO, SocialRegisterDTO } from "../dto";
 import { UserSessionRepository } from "../infra/repositories/user-session.repository";
 import { UserSocialRepository } from "../infra/repositories/user-social.repository";
 import { UserRepository } from "../infra/repositories/user.repository";
 import { UserSessionModel } from "../models/user-session.model";
 import { UserSocialModel } from "../models/user-social.model";
 import { UserModel } from "../models/user.model";
+import { generateRandomNickname } from "../utils/generate-random-nickname";
 
 @Injectable()
 export class SocialAuthService {
 
     constructor(
+        private readonly firebaseService: FirebaseService,
         private readonly mailService: MailService,
         private readonly secureTokenService: SecureTokenService,
         private readonly userSocialRepository: UserSocialRepository,
@@ -50,5 +52,42 @@ export class SocialAuthService {
 
         // 5. 토큰 리소스 반환
         return AuthResultDTO.fromSuccess(accessToken, expiresIn, refreshToken);
+    }
+
+    async register(socialId: string, dto: SocialRegisterDTO): Promise<void> {
+        return await this.firebaseService.runInTransaction(async () => {
+            // 1. 이메일 중복 채크
+            let user: UserModel = await this.userRepository.findUserByEmail(dto.email);
+            if (user) {
+                throw new BadRequestException('이미 가입된 이메일입니다.');
+            }
+
+            // 2. 소셜 중복 채크
+            let social: UserSocialModel = await this.userSocialRepository.findSocialBySocialId(socialId);
+            if (social) {
+                throw new BadRequestException('이미 가입된 소셜 계정입니다.');
+            }
+
+            // 3. 회원 모델 생성 및 저장
+            const randomNickname: string = generateRandomNickname();
+            user = UserModel.create({
+                email: dto.email,
+                nickname: randomNickname,
+            });
+            console.log(user);
+            await this.userRepository.save(user);
+
+            // 4. 소셜 회원 모델 생성 및 저장
+            social = UserSocialModel.create({
+                provider: dto.provider,
+                socialId: socialId,
+                userId: user.id,
+            });
+            console.log(social);
+            await this.userSocialRepository.save(social);
+
+            // 5. 이메일로 OTP 발송
+            await this.mailService.send(dto.email, user.otp);
+        });
     }
 }
