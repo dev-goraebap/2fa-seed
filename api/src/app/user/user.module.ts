33ae2f09ed95a05fbfa8cc704e50fb5856c10186
dev.goraebap <dev.goraebap@gmail.com>
2025-01-01 13:17:16 +0900

    Feat: update module

--- api/src/app/user/user.module.ts
+++ api/src/app/user/user.module.ts
@@ -1,22 +1,27 @@
 import { Module } from "@nestjs/common";
 
 import { UserSessionRepository } from "./infra/repositories/user-session.repository";
+import { UserSocialRepository } from "./infra/repositories/user-social.repository";
 import { UserRepository } from "./infra/repositories/user.repository";
-import { AuthService } from "./services/auth.service";
+import { LocalAuthService } from "./services/local-auth.service";
 import { UserSessionService } from "./services/user-session.service";
 import { UserService } from "./services/user.service";
+import { SocialAuthService } from "./services/social-auth.service";
 
 @Module({
     imports: [],
     providers: [
         UserRepository,
         UserSessionRepository,
-        AuthService,
+        UserSocialRepository,
+        LocalAuthService,
+        SocialAuthService,
         UserService,
         UserSessionService,
     ],
     exports: [
-        AuthService,
+        LocalAuthService,
+        SocialAuthService,
         UserService,
         UserSessionService,
     ]
