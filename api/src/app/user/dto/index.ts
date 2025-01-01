99a7cdbd1f33f2dae1a07794251b932e416bce6b
dev.goraebap <dev.goraebap@gmail.com>
2025-01-01 13:17:00 +0900

    Feat: update export file

--- api/src/app/user/dto/index.ts
+++ api/src/app/user/dto/index.ts
@@ -1,5 +1,4 @@
 export * from './create-device.dto';
-export * from './login.dto';
 export * from './logout-other-device.dto';
 export * from './register.dto';
 export * from './res/auth-result.dto';
@@ -7,5 +6,7 @@ export * from './res/device-result.dto';
 export * from './res/email-duplicate-check-result.dto';
 export * from './res/profile-result.dto';
 export * from './retry-otp.dto';
+export * from './social-register.dto';
 export * from './update-nickname.dto';
 export * from './verify-otp.dto';
+
