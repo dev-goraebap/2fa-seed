a5242dedd2962c2c25a6ee267090188d7292ff7d
dev.goraebap <dev.goraebap@gmail.com>
2025-01-01 13:16:46 +0900

    Feat: update dto

--- api/src/app/user/dto/register.dto.ts
+++ api/src/app/user/dto/register.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsNotEmpty, Length, Matches } from "class-validator";
 
-import { RegisterDTO as TRegisterDTO, USER_RULES } from 'domain-shared/user';
+import { LoginDTO as TLoginDTO, RegisterDTO as TRegisterDTO, USER_RULES } from 'domain-shared/user';
 
 export class RegisterDTO implements TRegisterDTO {
     @IsNotEmpty({ message: '이메일은 필수 입력 항목입니다.' })
@@ -14,4 +14,10 @@ export class RegisterDTO implements TRegisterDTO {
     @Matches(USER_RULES.password.regex, { message: USER_RULES.password.regexErrMsg })
     @ApiProperty({ description: '비밀번호', example: '1q2w3e1!@' })
     readonly password: string;
+}
+
+export class LoginDTO extends RegisterDTO implements TLoginDTO {
+    @IsNotEmpty({ message: '디바이스ID가 필요합니다.' })
+    @ApiProperty({ description: '디바이스ID' })
+    readonly deviceId: string;
 }
