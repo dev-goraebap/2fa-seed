5bc6834886591c486cff873378ba73cf3f826e07
dev.goraebap <dev.goraebap@gmail.com>
2025-01-01 13:17:46 +0900

    Feat: update export file

--- api/src/api/controllers/index.ts
+++ api/src/api/controllers/index.ts
@@ -1,4 +1,5 @@
 export * from './app.controller';
 export * from './auth.controller';
 export * from './device-access.controller';
+export * from './social.controller';
 export * from './user.controller';
