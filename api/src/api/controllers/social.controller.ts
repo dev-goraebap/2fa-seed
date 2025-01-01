1d3b67e124a913873877dc0a233ebd25a1d79708
dev.goraebap <dev.goraebap@gmail.com>
2025-01-01 20:01:14 +0900

    Feat: update social controller

--- api/src/api/controllers/social.controller.ts
+++ api/src/api/controllers/social.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Get, Post, Query, Render, Res } from "@nestjs/common";
-import { ApiOperation, ApiTags } from "@nestjs/swagger";
+import { Body, Controller, Get, HttpStatus, Post, Query, Render, Res } from "@nestjs/common";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { Response } from "express";
 
 import { AuthResultDTO, SocialAuthService, SocialLoginDTO, SocialRegisterDTO } from "src/app/user";
@@ -63,7 +63,8 @@ export class SocialController {
     }
 
     @Post('login')
-    @ApiOperation({ summary: '소셜 로그인' })
+    @ApiOperation({ summary: '소셜 로그인', description: '새로운 디바이스에서 로그인시 `새로운 디바이스 액세스 API` 연계 필요' })
+    @ApiResponse({ type: AuthResultDTO, description: '로그인 성공 (새로운 디바이스 로그인 시 이메일로 OTP 자동 발송)' })
     async login(@Body() dto: SocialLoginDTO): Promise<AuthResultDTO> {
         let profile: OAuthProfileResultDTO;
         if (dto.provider === OAuthProviders.KAKAO) {
@@ -76,8 +77,16 @@ export class SocialController {
     }
 
     @Post('register')
-    @ApiOperation({ summary: '소셜 회원가입' })
+    @ApiOperation({ summary: '소셜 회원가입', description: '회원가입 성공 시 `새로운 디바이스 액세스 API` 연계 필요' })
+    @ApiResponse({ status: HttpStatus.CREATED, description: '회원가입 성공 (이메일로 OTP 자동 발송)' })
     async register(@Body() dto: SocialRegisterDTO) {
+        let profile: OAuthProfileResultDTO;
+        if (dto.provider === OAuthProviders.KAKAO) {
+            profile = await this.kakaoService.oauthGetProfile(dto.accessTokenOrIdToken);
+        } else if (dto.provider === OAuthProviders.GOOGLE) {
+            profile = await this.googleService.oauthGetProfile(dto.accessTokenOrIdToken);
+        }
 
+        return await this.socialAuthService.register(profile.socialId, dto);
     }
 }
