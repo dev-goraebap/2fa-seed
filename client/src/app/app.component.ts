1f605b9d527c49347cb6ab0708c316690702375d
dev.goraebap <dev.goraebap@gmail.com>
2025-01-02 21:08:33 +0900

    Feat: update app component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,8 @@
-import { HttpErrorResponse } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { catchError, EMPTY, tap } from 'rxjs';
 
-import { HealthCheckService } from 'src/entities/health-check';
 import { environment } from 'src/shared/environments';
-import { TokenRefreshService } from 'src/shared/libs/jwt/plain-functions/token-refresh.service';
+import { TokenRefreshService } from 'src/shared/libs/jwt';
 
 @Component({
   selector: 'app-root',
@@ -14,18 +11,8 @@ import { TokenRefreshService } from 'src/shared/libs/jwt/plain-functions/token-r
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  private readonly healthCheckService: HealthCheckService = inject(HealthCheckService);
-
   constructor() {
     const tokenRefreshService = TokenRefreshService.getInstance();
     tokenRefreshService.initRefreshApiUrl(`${environment.apiUrl}/v1/auth/refresh`);
-
-    this.healthCheckService.healthCheck().pipe(
-      tap(console.log),
-      catchError((res: HttpErrorResponse) => {
-        console.log(res.error);
-        return EMPTY;
-      })
-    ).subscribe();
   }
 }
