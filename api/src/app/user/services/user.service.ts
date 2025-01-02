9784e7bd9ef6a9b6a5f5bc42a759f5230e2cd3c4
dev.goraebap <dev.goraebap@gmail.com>
2025-01-02 17:04:27 +0900

    Feat: update service

--- api/src/app/user/services/user.service.ts
+++ api/src/app/user/services/user.service.ts
@@ -1,8 +1,7 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 
 import { FirebaseService } from "src/shared/third-party";
 
-import { WithdrawDTO } from "../dto";
 import { UserRepository } from "../infra/repositories/user.repository";
 import { UserModel } from "../models/user.model";
 
@@ -14,17 +13,26 @@ export class UserService {
         private readonly userRepository: UserRepository,
     ) { }
 
+    async getUserByEmailOrThrow(email: string): Promise<UserModel> {
+        const user: UserModel = await this.userRepository.findUserByEmail(email);
+        if (!user) {
+            throw new BadRequestException('사용자를 찾을 수 없습니다.');
+        }
+        return user;
+    }
+
     async updateNickname(user: UserModel, nickname: string): Promise<UserModel> {
         user = user.withUpdateNickname(nickname);
         await this.userRepository.save(user);
         return user;
     }
 
-    async withdraw(user: UserModel, dto: WithdrawDTO, callback: () => Promise<void>): Promise<void> {
-        if (!user.verifyOtp(dto.otp)) {
-            throw new UnauthorizedException('OTP 코드가 유효하지 않습니다.');
-        }
+    async updatePassword(user: UserModel, password: string): Promise<void> {
+        user = user.withUpdatePassword(password);
+        await this.userRepository.save(user);
+    }
 
+    async withdraw(user: UserModel, callback: () => Promise<void>): Promise<void> {
         this.firebaseService.runInTransaction(async () => {
             await this.userRepository.remove(user);
             await callback();
