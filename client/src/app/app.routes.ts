3740477cb634460f1686e1fdda35030c8e93c64e
dev.goraebap <dev.goraebap@gmail.com>
2025-01-02 21:09:19 +0900

    Feat: update app routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -9,24 +9,6 @@ export const routes: Routes = [
     },
     {
         path: 'login',
-        loadComponent: () => import('../pages/public/login').then(m => m.LoginPage)
-    },
-    {
-        path: 'register',
-        loadComponent: () => import('../pages/public/register').then(m => m.RegisterPage)
-    },
-    {
-        path: 'profile',
-        canActivate: [jwtGuard],
-        loadComponent: () => import('../pages/private/profile').then(m => m.ProfilePage)
-    },
-    {
-        path: 'profile/edit',
-        canActivate: [jwtGuard],
-        loadComponent: () => import('../pages/private/edit-profile').then(m => m.EditProfilePage)
-    },
-    {
-        path: 'error',
-        loadComponent: () => import('../pages/public/error').then(m => m.ErrorPage)
+        loadComponent: () => import('../pages/login').then(m => m.LoginPage)
     }
 ];
