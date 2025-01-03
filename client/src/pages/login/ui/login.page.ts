cb4478794727924789faa3ae38576705420d8364
dev.goraebap <dev.goraebap@gmail.com>
2025-01-03 15:33:37 +0900

    Feat: update login page

--- client/src/pages/login/ui/login.page.ts
+++ client/src/pages/login/ui/login.page.ts
@@ -1,10 +1,10 @@
 import { HttpErrorResponse } from "@angular/common/http";
 import { Component, inject, Signal, viewChild } from "@angular/core";
-import { RouterLink } from "@angular/router";
+import { Router, RouterLink } from "@angular/router";
 import { Notyf } from "notyf";
-import { catchError, EMPTY, finalize } from "rxjs";
+import { catchError, EMPTY, finalize, tap } from "rxjs";
 
-import { LoginDTO } from "domain-shared/user";
+import { AuthStatus, LoginDTO } from "domain-shared/user";
 import { AuthService } from "src/entities/user";
 import { LoginForm, LoginFormDTO } from "src/features/login";
 import { Browser } from "src/shared/libs/browser";
@@ -21,16 +21,28 @@ export class LoginPage {
 
     protected showPasswordFindButton: boolean = false;
 
-    private readonly authService: AuthService = inject(AuthService);
+    private readonly router: Router = inject(Router);
     private readonly loginFormEl: Signal<LoginForm> = viewChild.required('loginForm');
+    private readonly authService: AuthService = inject(AuthService);
 
-    protected onLogin(dto: LoginFormDTO) {
+    protected onLogin(dto: LoginFormDTO): void {
         const deviceId: string = Browser.getId();
         const loginDTO: LoginDTO = {
             ...dto,
             deviceId
         }
         this.authService.login(loginDTO).pipe(
+            tap(res => {
+                if (res.status === AuthStatus.NEED_OTP) {
+                    this.router.navigateByUrl('/verify-otp', {
+                        state: {
+                            email: dto.email
+                        }
+                    });
+                } else {
+                    this.router.navigateByUrl('/users/me');
+                }
+            }),
             catchError((res: HttpErrorResponse) => {
                 // Easter egg 🐣
                 this.showPasswordFindButton = true;
