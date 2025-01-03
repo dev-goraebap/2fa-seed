8e24918a797b919b7d3c802d34829a214f706377
dev.goraebap <dev.goraebap@gmail.com>
2025-01-03 13:20:58 +0900

    Feat: update register form

--- client/src/features/register/ui/register.form.ts
+++ client/src/features/register/ui/register.form.ts
@@ -47,8 +47,6 @@ export class RegisterForm extends FormHelper {
                 Validators.required,
                 this.confirmPasswordValidator()
             ]),
-        }, {
-            validators: []
         });
     }
 
