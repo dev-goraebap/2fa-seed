ec08694a7146e24aa401b2fa9af43124c52e3955
dev.goraebap <dev.goraebap@gmail.com>
2025-01-05 14:04:54 +0900

    Feat: update export file

--- client/src/entities/user/index.ts
+++ client/src/entities/user/index.ts
@@ -2,3 +2,4 @@ export * from './services/auth.service';
 export * from './services/device.service';
 export * from './services/user.service';
 export * from './states/profile.state';
+export * from './ui/profile.card';
