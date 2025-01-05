ba78fe9eff81bd4061fe228833ce9e822040b3f9
dev.goraebap <dev.goraebap@gmail.com>
2025-01-05 16:44:38 +0900

    Feat: update modal overlay

--- client/src/shared/ui/modal/modal.overlay.ts
+++ client/src/shared/ui/modal/modal.overlay.ts
@@ -3,9 +3,9 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'modal-overlay',
     template: `
-    <div class="fixed inset-0 bg-black/50 z-50 flex justify-center items-center">
+    <div class="fixed inset-0 bg-black/50 z-50 flex justify-center items-center motion-preset-fade">
         <div class="bg-base-100 p-4 w-full rounded-none sm:rounded-md sm:max-w-md h-full sm:h-auto
-        motion-opacity-in-[0] motion-translate-y-in-[5%] motion-duration-[0.7s] motion-ease-in-out">
+        motion-preset-expand motion-duration-200 motion-delay-200">
             <ng-content></ng-content>
         </div>
     </div>
