0adad71477f4b0a96be3a786cd1b0b4c524fe487
dev.goraebap <dev.goraebap@gmail.com>
2025-01-05 16:38:48 +0900

    Feat: update otp verify page

--- client/src/pages/otp-verify/ui/otp-verify.page.ts
+++ client/src/pages/otp-verify/ui/otp-verify.page.ts
@@ -1,10 +1,8 @@
-import { NgClass } from "@angular/common";
 import { Component, effect, inject, signal, WritableSignal } from "@angular/core";
 import { Router } from "@angular/router";
 import { Notyf } from "notyf";
-import { interval, tap } from "rxjs";
 
-import { OtpVerifyForm, OtpVerifyState } from "src/features/verify-otp";
+import { OtpRetryTimer, OtpVerifyForm, OtpVerifyState } from "src/features/verify-otp";
 import { CustomError } from "src/shared/services";
 
 @Component({
@@ -12,7 +10,7 @@ import { CustomError } from "src/shared/services";
     templateUrl: './otp-verify.page.html',
     imports: [
         OtpVerifyForm,
-        NgClass
+        OtpRetryTimer
     ],
     providers: [
         OtpVerifyState
@@ -24,7 +22,6 @@ export class OtpVerifyPage {
     private readonly otpVerifyState: OtpVerifyState = inject(OtpVerifyState);
 
     protected readonly email: WritableSignal<string | null> = signal(null);
-    protected canRetryTime: number = 30;
 
     constructor() {
         const state = this.router.getCurrentNavigation()?.extras.state;
@@ -53,34 +50,5 @@ export class OtpVerifyPage {
                 });
             }
         });
-
-        interval(1000).pipe(
-            tap(() => {
-                if (this.canRetryTime !== 0) {
-                    this.canRetryTime--;
-                }
-            })
-        ).subscribe();
-    }
-
-    onResendOtp(): void {
-        // this.authService.sendOtp(this.email).pipe(
-        //     tap(() => {
-        //         const notify = new Notyf();
-        //         notify.success({
-        //             message: '인증번호를 재전송하였습니다.',
-        //             dismissible: true
-        //         });
-        //     }),
-        //     catchError((res: HttpErrorResponse) => {
-        //         const notify = new Notyf();
-        //         notify.error({
-        //             message: res.error.message,
-        //             dismissible: true
-        //         });
-        //         return EMPTY;
-        //     }),
-        //     finalize(() => this.canRetryTime = 30),
-        // ).subscribe();
     }
 }
