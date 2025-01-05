0b84eaf10b2a01c5c8b1ee9b36c8b99d3d68df87
dev.goraebap <dev.goraebap@gmail.com>
2025-01-05 14:03:48 +0900

    Feat: create nickname edit modal

---
+++ client/src/widgets/nickname-edit-modal/index.ts
@@ -0,0 +1,1 @@
+export * from './ui/nickname-edit.modal';
---
+++ client/src/widgets/nickname-edit-modal/ui/nickname-edit.modal.ts
@@ -0,0 +1,56 @@
+import { Component, effect, inject } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { Notyf } from "notyf";
+
+import { ProfileResultDTO } from "domain-shared/user";
+import { ProfileState } from "src/entities/user";
+import { NicknameEditForm, NicknameEditState } from "src/features/nickname-edit";
+import { CustomError } from "src/shared/services";
+import { BaseModal, ModalOverlay } from "src/shared/ui";
+
+@Component({
+    selector: 'nickname-edit-modal',
+    templateUrl: './nickname-edit.modal.html',
+    imports: [
+        ReactiveFormsModule,
+        ModalOverlay,
+        NicknameEditForm
+    ],
+    providers: [
+        NicknameEditState
+    ]
+})
+export class NicknameEditModal extends BaseModal {
+
+    private readonly nicknameEditState: NicknameEditState = inject(NicknameEditState);
+    private readonly profileState: ProfileState = inject(ProfileState);
+
+    constructor() {
+        super();
+
+        effect(() => {
+            const data: ProfileResultDTO | null = this.nicknameEditState.data();
+            if (data) {
+                this.profileState.setData(data);
+                this.close();
+            }
+        });
+
+        effect(() => {
+            const error: CustomError | null = this.nicknameEditState.error();
+            if (error) {
+                const notyf = new Notyf();
+                notyf.error({
+                    message: error.message,
+                    dismissible: true
+                });
+            }
+        });
+    }
+
+    onCancel() {
+        const result = window.confirm('정말 취소하시겠습니까?');
+        if (!result) return;
+        this.close();
+    }
+}
